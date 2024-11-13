39902b9a4f654adf384e78df38c0d78bc165b144
Phát Lee <vanphat15it@gmail.com>
2024-11-13 09:22:34 +0700

    add signup function , add cookie/session to login

--- src/main/java/gr/careplus4/controllers/LoginController.java
+++ src/main/java/gr/careplus4/controllers/LoginController.java
@@ -43,8 +43,9 @@ public class LoginController {
 
     @PostMapping("/login/login-submit")
     public String checkLogin(@RequestParam("phoneNumber") String phoneNumber,
-                        @RequestParam("password") String password,
-                        Model model, HttpSession session, HttpServletResponse resp) {
+                             @RequestParam("password") String password,
+                             @RequestParam(value = "remember-me", required = false) String rememberMe, // Add remember-me parameter
+                             Model model, HttpSession session, HttpServletResponse resp) {
         String apiUrl = apibaseUrl + "/user/getUserLogin";
 
         // Tạo request gửi đến API đăng nhập
@@ -57,15 +58,18 @@ public class LoginController {
             ResponseEntity<Response> response = restTemplate.postForEntity(apiUrl, requestParams, Response.class);
             if (response.getBody() != null && response.getBody().getStatus()) {
                 // Đăng nhập thành công, chuyển hướng đến trang chủ
-                // Tạo Cookie cho mã định danh hoặc token của người dùng
-                Cookie userCookie = new Cookie("phoneNumber", phoneNumber);
-                userCookie.setHttpOnly(true); // Đảm bảo cookie chỉ được gửi trong các yêu cầu HTTP
-                userCookie.setPath("/"); // Đặt path cho cookie (sử dụng trên toàn bộ domain)
-                userCookie.setMaxAge(7 * 24 * 60 * 60); // Thời hạn cookie (ví dụ: 1 tuần)
-                resp.addCookie(userCookie);
 
-                // Lưu thông tin người dùng vào session
-                session.setAttribute("phoneNumber", phoneNumber);
+                if ("on".equals(rememberMe)) { // Only if remember-me is checked
+                    // Tạo Cookie cho mã định danh hoặc token của người dùng
+                    Cookie userCookie = new Cookie("phoneNumber", phoneNumber);
+                    userCookie.setHttpOnly(true); // Đảm bảo cookie chỉ được gửi trong các yêu cầu HTTP
+                    userCookie.setPath("/"); // Đặt path cho cookie (sử dụng trên toàn bộ domain)
+                    userCookie.setMaxAge(7 * 24 * 60 * 60); // Thời hạn cookie (ví dụ: 1 tuần)
+                    resp.addCookie(userCookie);
+
+                    // Lưu thông tin người dùng vào session
+                    session.setAttribute("phoneNumber", phoneNumber);
+                }
 
                 return "redirect:/";
             } else {
@@ -75,71 +79,59 @@ public class LoginController {
             }
         } catch (Exception e) {
             // Xử lý nếu có lỗi từ API hoặc kết nối thất bại
-            model.addAttribute("errorMessage", "An error occurred. Please try again. "
-                    + e.getMessage());
+            model.addAttribute("errorMessage", "An error occurred. Please try again.");
             return "login";
         }
     }
 
-//    @PostMapping("/au/login/login-submit")
-//    public ModelAndView loginSubmit(@Valid @ModelAttribute("userDTO") UserModel userDTO,
-//                                    HttpServletRequest request,
-//                                    HttpServletResponse response,
-//                              BindingResult bindingResult, Model model) {
-//        // Kiểm tra lỗi validate của form
-//        if (bindingResult.hasErrors()) {
-//            return new ModelAndView("redirect:/au/login");
-//        }
-//
-//        // Kiểm tra thông tin đăng nhập
-//        if (!userService.checkLogin(userDTO.getPhoneNumber(), userDTO.getPassword())) {
-//            model.addAttribute("errorMessage", "Số diện thoại hoặc mật khẩu không đúng." + userDTO.getPhoneNumber() + userDTO.getPassword());
-//            return new ModelAndView("login"); // Trả về trang đăng nhập nếu thông tin không đúng
-//        }
-//        // Đăng nhập thành công, lưu số điện thoại vào session
-//        HttpSession session = request.getSession();
-//        session.setAttribute("phoneNumber", userDTO.getPhoneNumber());
-//        System.out.println("phoneNumber: " + userDTO.getPhoneNumber());
-//        System.out.println("remember-me: " + request.getParameter("remember-me"));
-//
-//        // Nếu người dùng chọn "Remember Me", tạo cookie lưu số điện thoại
-//        if ("on".equals(request.getParameter("remember-me"))) {
-//            Cookie cookie = new Cookie("phoneNumber", userDTO.getPhoneNumber());
-//            cookie.setMaxAge(7 * 24 * 60 * 60); // Cookie tồn tại trong 7 ngày
-//            cookie.setPath("/"); // Đảm bảo cookie có thể được gửi trên tất cả các URL trong domain
-//            response.addCookie(cookie); // Thêm cookie vào response
-//        }
-//        model.addAttribute("successMessage", "Đăng nhập thành công!");
-//        return new ModelAndView("redirect:/"); // Chuyển đến trang đăng nhập sau khi đăng ký thành công
-//    }
 
-    @GetMapping("/au/signup")
+    @GetMapping("/signup")
     public String signup() {
         return "signup";
     }
 
-//    @PostMapping("/au/signup/signup-submit")
-//    public ModelAndView signupSubmit(@Valid @ModelAttribute("user") User user,
-//                                     BindingResult bindingResult, Model model) {
-//        // Kiểm tra lỗi validate của form
-//        if (bindingResult.hasErrors()) {
-//            return new ModelAndView("signup");
-//        }
-//
-//        // Kiểm tra xem email đã tồn tại chưa
-//        if (userService.emailExists(user.getEmail())) {
-//            model.addAttribute("errorMessage", "Email đã được sử dụng.");
-//            return new ModelAndView("signup"); // Trả về lại trang đăng ký với thông báo lỗi
-//        }
-//
-//        try {
-//            // Lưu người dùng vào database thông qua UserService
-//            userService.createUser(user);
-//            model.addAttribute("successMessage", "Đăng ký thành công!");
-//            return new ModelAndView("redirect:/au/login"); // Chuyển đến trang đăng nhập sau khi đăng ký thành công
-//        } catch (Exception e) {
-//            model.addAttribute("errorMessage", "Có lỗi xảy ra khi đăng ký. Vui lòng thử lại." + user.toString());
-//            return new ModelAndView("signup"); // Trả về trang đăng ký nếu có lỗi
-//        }
-//    }
+    @PostMapping("/signup/signup-submit")
+    public String signup(@RequestParam("name") String name,
+                         @RequestParam("phoneNumber") String phoneNumber,
+                         @RequestParam("password") String password,
+                         @RequestParam("re_pass") String rePass,
+                         @RequestParam("gender") String gender,
+                         @RequestParam("email") String email,
+                         @RequestParam("address") String address,
+                         @RequestParam("role") int role,
+                         Model model) {
+        // Kiểm tra xác nhận mật khẩu
+        if (!password.equals(rePass)) {
+            model.addAttribute("errorMessage", "Mật khẩu và xác nhận mật khẩu không khớp.");
+            return "signup";
+        }
+
+        String apiUrl = apibaseUrl + "/user/addUser";
+
+        // Tạo request để gửi đến API
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("name", name);
+        requestParams.add("phoneNumber", phoneNumber);
+        requestParams.add("password", password);
+        requestParams.add("gender", gender);
+        requestParams.add("email", email);
+        requestParams.add("address", address);
+        requestParams.add("role", String.valueOf(role));
+
+        try {
+            // Gửi POST request tới API
+            ResponseEntity<Response> response = restTemplate.postForEntity(apiUrl, requestParams, Response.class);
+
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null && response.getBody().getStatus()) {
+                model.addAttribute("successMessage", "Đăng ký thành công! Bạn có thể đăng nhập ngay.");
+                return "redirect:/au/login"; // Điều hướng đến trang đăng nhập khi thành công
+            } else {
+                model.addAttribute("errorMessage", "Số điện thoại hoặc email đã tồn tại.");
+                return "signup"; // Trả về trang đăng ký nếu thất bại
+            }
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Đã xảy ra lỗi. Vui lòng thử lại sau. ");
+            return "signup"; // Trả về trang đăng ký nếu có lỗi
+        }
+    }
 }
