28f1adeb3c73097e5cb91e03fe9c88304a38ad74
lightningflashh <thanhchi0429@gmail.com>
2024-11-15 23:09:02 +0700

    cart & cartdetail api & fix entity

--- src/main/java/gr/careplus4/models/CartConfirmModel.java
+++ src/main/java/gr/careplus4/models/CartConfirmModel.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CartModel {
+public class CartConfirmModel {
     private String id;
     private String userPhoneNumber;
     private int productCount;
+    private List<CartDetailConfirmModel> cartDetails;
 }
